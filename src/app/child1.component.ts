efd56aaaae436b35ebbde2b11ac681303f8bf595
amit.shah <amit.shah@avista.com>
2018-11-15 18:02:36 +0530

    #ng g component child1 --flat --spec=false -s -t
    Created new component child1.

---
+++ src/app/child1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child1',
+  template: `
+    <p>
+      child1 works!
+    </p>
+  `,
+  styles: []
+})
+export class Child1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
