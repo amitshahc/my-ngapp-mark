3c0e6dc32610a6d4c215b213122aed3d6aa2d043
amit.shah <amit.shah@avista.com>
2018-11-19 17:04:06 +0530

    Parent-Child Communication example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { Child1input } from './interfaces/child1input';
+import {
+  Component
+} from '@angular/core';
+import {
+  Child1input
+} from './interfaces/child1input';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +13,31 @@ import { Child1input } from './interfaces/child1input';
 export class AppComponent {
   title = 'learnAngular';
   ParentInput: Child1input = {
-    title: 'Parent Title',
+    title: 'Initial Parent Title',
     description: 'Parent Description ...'
   }
+  private cntClick = 0;
 
   constructor() {
     console.clear();
   }
 
   ngOnInit() {
-    setTimeout(() => {
-      this.ParentInput.title = 'new Title';
-    }, 2000)
+    // setTimeout(function () {
+    //   this.ParentInput.title = 'New Updated Title';
+    // }.bind(this), 2000);
+
+    // setTimeout(
+    //   () => console.table('ParentInput: ',
+    //     this.ParentInput.title), 7000
+    // );
+  }
+
+  parentCatchUpdate(clicked: boolean) {
+    //console.table(childInput);
+    if (clicked) {
+      this.cntClick++;
+      this.ParentInput.title = 'Child updated the title '+this.cntClick+' times';  
+    }    
   }
 }
--- src/app/child1.component.ts
+++ src/app/child1.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Child1input } from './interfaces/child1input';
+//import { EventEmitter } from 'events';
 
 @Component({
   selector: 'myapp-child1',
@@ -7,6 +8,7 @@ import { Child1input } from './interfaces/child1input';
     <p>
       {{localInput.title}}<br />
       {{localInput.description}}
+      <button (click)="sendUpdatedTitle()">Update Title</button>
     </p>
   `,
   styles: []
@@ -14,9 +16,21 @@ import { Child1input } from './interfaces/child1input';
 export class Child1Component implements OnInit {
 
   @Input('AliasInput') localInput: Child1input;
+  @Output() updateParentEvent = new EventEmitter<boolean>();
+  private btnClick: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+    // setTimeout(() => {
+    //   this.localInput.title = "Child updated Title";
+    //   this.sendUpdatedTitle();
+    // }, 5000);
   }
 
+  sendUpdatedTitle() {
+    //console.log('sendUpdatedTitle()');
+    this.btnClick = true;
+    this.updateParentEvent.emit(this.btnClick);
+  }
 }
